55bc25e883da90e7b670671b759bdebdca9a2b81
Otis Lau <otishlau@gmail.com>
2025-12-20 13:03:54 -0500

    Initial Commit

---
+++ screenmates/screenmatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenmatesApp.swift
+//  screenmates
+//
+//  Created by Otis Lau on 2025-12-20.
+//
+
+import SwiftUI
+
+@main
+struct screenmatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
